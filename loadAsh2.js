18d5202f890c158d9bc819379e04573f9eb35a7d
ashwinKumar0505 <53852911+ashwinKumar0505@users.noreply.github.com>
2019-08-09 10:27:00 +0530

    modified Load-Ash2

--- loadAsh2.js
+++ loadAsh2.js
@@ -29,42 +29,51 @@ isEmpty=(array)=>{
 isEqual=(obj,obj1)=>{
 let flag=0;
 
-let obj_keys=Object.keys(obj)
+obj_keys=Object.keys(obj)
 
-let obj_size=obj_keys.length
+obj_size=obj_keys.length
 
-let obj1_keys=Object.keys(obj1)
+obj1_keys=Object.keys(obj1)
 
-let obj1_size=obj1_keys.length
+obj1_size=obj1_keys.length
 
 if(obj_size==obj1_size){
 for(let key in obj){
   if(obj1.hasOwnProperty(key)){
-       
-       if(obj[key]!=obj1[key]){
-         flag=1;
-         break;
+       if(typeof obj[key]=='object'&&typeof obj1[key]=='object'){
+          value=isEqual(obj[key],obj1[key])
+          if(value==false){
+            return false
+          }
        }
+        else{
+         if(obj[key]!=obj1[key]){
+         flag=1;
+         }
+       }
+      
   }
-  else{
+  else
+  {
     flag=1;
     break
   }
 }
-
 }
-
 else{
 flag=1;
-}
-
+ }
 if(flag==0){
   return true
-}
+ }
 else{
 return false;
+ }
 }
-}
+
+
+
+
 
 
 reverseString=(string)=>{
@@ -110,22 +119,24 @@ console.log(isArray("ashwin"))
 
 
 
-let obj={name:"ashwin",age:30,add:'jsjd'}
+let obj={name:"ashwin",age:30,add:"hids"}
 
-let obj1={name:"ashwin",ae:30}
+let obj1={add:"hids",age:30,name:"ashwin"}
 
-let obj2={name:"ashwin",age:35};
+let obj2={name:"ashwin",b:{age:35}};
 
-let obj3={name:'ashwin',age:30};
+let obj3={name:'ashwin',b:{age:35}};
 
 let obj4={name:"ashwin",age:30};
+
+let obj5={name:'ashwin',age:30};
 
 
 console.log(isEqual(obj,obj1))
 
 console.log(isEqual(obj2,obj3))
 
-console.log(isEqual(obj4,obj3))
+console.log(isEqual(obj4,obj5))
 
 
 
